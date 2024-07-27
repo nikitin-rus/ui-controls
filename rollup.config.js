301f689a7b7e1e2decf8282888052382b9096531
Nikitin Ruslan <mystudychannel.0@gmail.com>
2024-07-27 18:08:46 +0300

    configure project

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,29 @@
-import nodeResolve from "@rollup/plugin-node-resolve";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
+
 import postcss from 'rollup-plugin-postcss';
 
-export default [
-    {
-        input: "src/index.ts",
-        output: [
-            {
-                file: "dist/cjs/index.js",
-                format: "cjs",
-                sourcemap: true,
-            },
-            {
-                file: "dist/esm/index.js",
-                format: "esm",
-                sourcemap: true,
-            },
-        ],
-        plugins: [
-            nodeResolve(),
-            commonjs(),
-            typescript({ tsconfig: "./tsconfig.json" }),
-            postcss({
-                extract: false,
-                modules: true,
-                use: ["sass"]
-            }),
-        ]
+export default {
+    input: "src/index.ts",
+    output: {
+        file: "dist/index.js",
+        format: "esm",
+        sourcemap: true,
     },
-    {
-        input: "dist/esm/types/index.d.ts",
-        output: [{ file: "dist/index.d.ts", format: "esm" }],
-        plugins: [dts()],
-        external: [/\.css$/],
-    }
-];
+    plugins: [
+        nodeResolve(),
+        commonjs(),
+        typescript({
+            tsconfig: "./tsconfig.json",
+            declaration: true,
+            declarationDir: 'dist',
+        }),
+        postcss({
+            extract: false,
+            modules: true,
+            use: ["sass"]
+        }),
+    ],
+    external: ['react', 'react-dom'],
+};
